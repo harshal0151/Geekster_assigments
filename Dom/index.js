baf717950504e7823e9e09810836f79b4622974c
harshal0151 <patilharshal0151@gmail.com>
2024-04-12 16:53:39 +0530

    Dom

---
+++ Dom/index.js
@@ -0,0 +1,14 @@
+
+const targetDiv = document.getElementById('targetDiv');
+
+// Change background color to yellow
+targetDiv.addEventListener("click", () => {
+  
+targetDiv.style.backgroundColor = 'yellow';
+targetDiv.style.margin = '20px';
+targetDiv.style.padding = '10px';
+targetDiv.style.fontSize = '18px';
+targetDiv.style.fontWeight = 'bold';
+targetDiv.style.height = '200px';
+targetDiv.style.width = '300px';
+});
