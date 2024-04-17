9f371f09cfd477b1bee2946abcfca35ebb13ed96
harshal0151 <patilharshal0151@gmail.com>
2024-04-17 21:48:29 +0530

    js Mode change to lite and dark

---
+++ js-lightAndDark/index.js
@@ -0,0 +1,14 @@
+const on = document.querySelector("#onBtn");
+const output = document.querySelector("body");
+
+let darkMode =  output.style.backgroundColor = "black"; 
+let Mode =  output.style.backgroundColor = "white"; 
+
+on.addEventListener('click' , ChangeMode) ;
+
+function ChangeMode (){
+    if( output.style.backgroundColor = "white"  ){
+        output.style.backgroundColor = "white" ;
+  
+}
+}
