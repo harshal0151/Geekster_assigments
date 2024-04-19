8a03beb05da8db7705690a5ff51d3da3b35c72d5
harshal0151 <patilharshal0151@gmail.com>
2024-04-19 10:53:06 +0530

    update code

--- js-lightAndDark/index.js
+++ js-lightAndDark/index.js
@@ -1,14 +1,42 @@
-const on = document.querySelector("#onBtn");
-const output = document.querySelector("body");
+// const on = document.querySelector("#onBtn");
+// const output = document.querySelector("body");
+// const text = document.querySelector("h1");
 
-let darkMode =  output.style.backgroundColor = "black"; 
-let Mode =  output.style.backgroundColor = "white"; 
 
-on.addEventListener('click' , ChangeMode) ;
+// on.addEventListener('click' , ChangeMode) ;
 
-function ChangeMode (){
-    if( output.style.backgroundColor = "white"  ){
-        output.style.backgroundColor = "white" ;
-  
-}
+// function ChangeMode (){
+//     if( output.style.backgroundColor === "white"  ){
+//         output.style.backgroundColor = "black" ;
+//         text.style.color = "white";
+// }else{
+//  output.style.backgroundColor = "white" ;
+//  text.style.color = "black";     
+// }
+// }
+
+
+
+var toggled = false; 
+
+var hTag = document.getElementsByTagName("h1")[0];
+var bodyTag = document.getElementsByTagName("body")[0];
+var circle = document.getElementById("circle");
+
+document.getElementById("toggle-container").onclick = function () {
+        
+    if(!toggled) {
+        hTag.classList.add("color-white");
+        bodyTag.classList.add("bck-color-black");
+        circle.classList.add("dark-mode")
+
+        toggled = true;
+    } else {
+        hTag.classList.remove("color-white");
+        bodyTag.classList.remove("bck-color-black");
+        circle.classList.remove("dark-mode")
+
+        toggled = false;
+    }
+    
 }
